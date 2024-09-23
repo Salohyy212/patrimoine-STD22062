edf10266c2118296af9ff4ef80659255569fb3a4
Salohyy212 <zerambaysalohy@gmail.com>
2024-09-23 14:29:42 +0300

    feat: generation project spring boot

---
+++ src/main/java/com/salohy/patrimoinestd22062/PatrimoineStd22062Application.java
@@ -0,0 +1,13 @@
+package com.salohy.patrimoinestd22062;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatrimoineStd22062Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatrimoineStd22062Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/salohy/patrimoinestd22062/PatrimoineStd22062ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salohy.patrimoinestd22062;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatrimoineStd22062ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
