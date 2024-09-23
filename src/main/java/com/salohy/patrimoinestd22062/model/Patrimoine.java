91f8d5d2a8f6eb5845314c4fd0ffab97ba561eec
Salohyy212 <zerambaysalohy@gmail.com>
2024-09-23 14:47:38 +0300

    feat: implement model Patrimoine

---
+++ src/main/java/com/salohy/patrimoinestd22062/model/Patrimoine.java
@@ -0,0 +1,32 @@
+package com.salohy.patrimoinestd22062.model;
+
+import java.time.LocalDateTime;
+
+public class Patrimoine {
+    private String possesseur;
+    private LocalDateTime derniereModification;
+
+    public Patrimoine() {}
+
+    public Patrimoine(String possesseur, LocalDateTime derniereModification) {
+        this.possesseur = possesseur;
+        this.derniereModification = derniereModification;
+    }
+
+    public String getPossesseur() {
+        return possesseur;
+    }
+
+    public void setPossesseur(String possesseur) {
+        this.possesseur = possesseur;
+    }
+
+    public LocalDateTime getDerniereModification() {
+        return derniereModification;
+    }
+
+    public void setDerniereModification(LocalDateTime derniereModification) {
+        this.derniereModification = derniereModification;
+    }
+}
+
